e612c2f6a84fcda3ec9d0c4e92aee6cd72e61225
OlgaShalaginova <olechkatsapova@gmail.com>
2023-01-27 19:24:12 +0500

    Telephone

---
+++ model_import.py
@@ -0,0 +1,11 @@
+import view
+
+data = view.input_data()
+print(data)
+def import_txt():
+    global data
+    with open('HW7Python/text.txt', 'a', encoding='utf-8') as file_data:
+        for i in range(len(data)):
+            file_data.writelines(f'{data[i]}'+';')
+
+import_txt()
